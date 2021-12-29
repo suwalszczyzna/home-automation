70a958055f131fa668062af3e7892d3c610d7b7c
Damian Suwała <damian.suwala@gmail.com>
2021-12-29 19:35:28 +0100

    catch all exceptions

--- app/entrypoints/scripts/update_sensor_loop.py
+++ app/entrypoints/scripts/update_sensor_loop.py
@@ -11,8 +11,8 @@ log = logger.get_logger("update-sensor-loop")
 def run_update_sensor():
     try:
         requests.get("http://127.0.0.1:5000/api/update_sensors_values", timeout=5)
-    except ReadTimeout:
-        log.error("Timeout")
+    except Exception as e:
+        log.error("Error", exc_info=e)
 
 
 if __name__ == '__main__':
