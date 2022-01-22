2e7a31b8ccce77194a70bf719391985af0c09dcf
Damian Suwala <damian.suwala@gmail.com>
2022-01-22 18:15:10 +0100

    frontend improvements

--- app/entrypoints/vue/app.js
+++ app/entrypoints/vue/app.js
@@ -2,7 +2,13 @@ Vue.createApp({
     data(){
         return {
             coTemp: 0,
-            waterTemp: 0
+            waterTemp: 0,
+
+            valveStatus: false,
+            heaterStatus: false,
+    
+            valveStatusText: "OFF",
+            heaterStatusText: "OFF",
         }
     },
     created(){
@@ -13,6 +19,26 @@ Vue.createApp({
                 this.coTemp = data.co_temp
                 this.waterTemp = data.water_temp
             })
-            .catch(err => console.error(err))
+            .catch(err => {
+                this.coTemp = -1
+                this.waterTemp = -1
+            })
+
+        fetch("http://192.168.1.25:5000/api/smart_device_statuses")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.heaterStatus = data.heater
+                this.valveStatus = data.valve
+
+                data.heater ? this.heaterStatusText = "ON" : this.heaterStatusText = "OFF"
+                data.valve ? this.valveStatusText = "ON" : this.valveStatusText = "OFF"
+
+            })
+            .catch(err => {
+                console.error(err)
+                this.heaterStatus = false
+                this.valveStatus = false
+            })
     }
 }).mount('#temp')
