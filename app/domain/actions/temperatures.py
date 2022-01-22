35c21c364f8b1ed39e61738279d954f4238666d4
damian <damian@debian-BULLSEYE-live-builder-AMD64>
2022-01-22 18:11:17 +0100

    added clean temp history action

--- app/domain/actions/temperatures.py
+++ app/domain/actions/temperatures.py
@@ -1,9 +1,15 @@
+import os
+from datetime import datetime, timedelta
+
 import inject
 
+import logger
 from app.domain.interfaces.abstract_database import AbstractDatabase
 
+log = logger.get_logger("TemperaturesActions")
 
-class Tempareratures:
+
+class Temperatures:
     @inject.autoparams()
     def __init__(self, db: AbstractDatabase):
         self._db = db
@@ -14,3 +20,8 @@ class Tempareratures:
             "co_temp": temp.co_temp,
             "water_temp": temp.water_temp,
         }
+
+    def clean_temp_history(self):
+        max_history = datetime.now() - timedelta(int(os.getenv('READ_SENSORS_INTERVAL')))
+        log.info("Cleaning temp history")
+        self._db.clean_temp_history(max_history)
