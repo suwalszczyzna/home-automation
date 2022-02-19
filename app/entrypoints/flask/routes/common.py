8a01bb41475d6ebeb4ae9efb452e9ce1b814337f
Damian Suwała <damian.suwala@gmail.com>
2022-02-19 23:41:48 +0100

    str2bool

--- app/entrypoints/flask/routes/common.py
+++ app/entrypoints/flask/routes/common.py
@@ -4,6 +4,8 @@ from flask import Blueprint, Response, jsonify, request
 from app.domain.actions.manage_operation_modes import ManageOperationModes
 from app.domain.actions.temperatures import Tempareratures
 
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
 
 @inject.autoparams()
 def create_common_blueprint(temp_actions: Tempareratures, manage_op: ManageOperationModes) -> Blueprint:
@@ -28,7 +30,7 @@ def create_common_blueprint(temp_actions: Tempareratures, manage_op: ManageOpera
     @common.route("/set_checking_low_cost")
     def set_checking_low_cost() -> Response:
         operation = request.args.get('operation')
-        should_check = request.args.get('value')
+        should_check = str2bool(request.args.get('value'))
         result = manage_op.set_checking_low_cost(operation, should_check)
         return jsonify(result)
 
