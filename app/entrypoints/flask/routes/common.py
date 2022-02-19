f7b854da4d918b0ece6503348d9962feba40e358
Damian Suwała <damian.suwala@gmail.com>
2022-02-19 19:01:20 +0100

    endpoint get_actual_mode

--- app/entrypoints/flask/routes/common.py
+++ app/entrypoints/flask/routes/common.py
@@ -1,16 +1,22 @@
 import inject
 from flask import Blueprint, Response, jsonify
 
+from app.domain.actions.manage_operation_modes import ManageOperationModes
 from app.domain.actions.temperatures import Tempareratures
 
 
 @inject.autoparams()
-def create_common_blueprint(temp_actions: Tempareratures) -> Blueprint:
+def create_common_blueprint(temp_actions: Tempareratures, manage_op: ManageOperationModes) -> Blueprint:
     common = Blueprint("common", __name__)
 
     @common.route("/temperature")
     def get_temperatures() -> Response:
         result = temp_actions.get_actual_temperatures()
+        return jsonify(result)
+
+    @common.route("/get_actual_mode")
+    def get_actual_mode() -> Response:
+        result = manage_op.get_active_operation_mode()
         return jsonify(result)
 
     return common
