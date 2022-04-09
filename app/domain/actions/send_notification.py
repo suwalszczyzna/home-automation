36796a8d10eb077282e79f21eafd42b6bb36ed20
Damian Suwała <damian.suwala@gmail.com>
2022-04-09 17:41:01 +0200

    added dryer notification

--- app/domain/actions/send_notification.py
+++ app/domain/actions/send_notification.py
@@ -5,6 +5,12 @@ from app.domain.interfaces.abstract_notification_api import AbstractNotification
 from app.domain.interfaces.abstract_database import AbstractDatabase
 
 
+messages = {
+    Devices.WASHER.value: "💧 Pralka uprała",
+    Devices.DRYER.value: "🐫 Suszarka skończyła pracę"
+}
+
+
 class SendNotification:
     @inject.autoparams()
     def __init__(self, api: AbstractNotificationApi, db: AbstractDatabase) -> None:
@@ -12,10 +18,12 @@ class SendNotification:
         self._db = db
     
     def send_notification(self):
-        device_name = Devices.WASHER.value
-        should_send_notification = self._db.get_notifier_status(device_name)
-        washer_current_power = self._db.get_current_power(device_name)
-        if should_send_notification and washer_current_power < 1:
-            chats = self._db.get_chats()
-            self._api.send_notification("🫧 Pralka uprała 🫧", chats)
-            self._db.set_notifier_status(device_name, False)
+        devices = [Devices.WASHER.value, Devices.DRYER.value]
+        for device_name in devices:
+            washer_current_power = self._db.get_current_power(device_name)
+            should_send_notification = self._db.get_notifier_status(device_name) and washer_current_power < 1
+
+            if should_send_notification:
+                chats = self._db.get_chats()
+                self._api.send_notification(messages.get(device_name, "😵"), chats)
+                self._db.set_notifier_status(device_name, False)
