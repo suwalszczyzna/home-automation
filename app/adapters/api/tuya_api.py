182ba232c5e0fbc38b367a8665e94b0c2a0af4bf
Damian Suwała <damian.suwala@gmail.com>
2022-04-11 17:57:22 +0200

    fix switch in tuya-api

--- app/adapters/api/tuya_api.py
+++ app/adapters/api/tuya_api.py
@@ -1,7 +1,9 @@
+from pprint import pprint
+
 from tinytuya import OutletDevice
 
 import logger
-from app.domain.devices import Device, Status, CurrentPower
+from app.domain.devices import Device, Status
 from app.domain.interfaces.abstract_device_api import AbstractDeviceAPI
 
 log = logger.get_logger("TuyaAPI")
@@ -20,25 +22,31 @@ class TuyaAPI(AbstractDeviceAPI):
         tuya_device.set_version(device.version)
         return tuya_device
 
+    def get_device_switch_status(self, tuya_device: OutletDevice):
+        tuya_status = tuya_device.status()
+        if tuya_status.get('Error'):
+            log.info("Can't get tuya status: %s", tuya_status)
+            return False
+        log.info(pprint(tuya_status))
+        return tuya_status['dps']['1']
+
     def set_switch_status(self, device: Device, status: Status) -> bool:
-        log.info("Chaning status for dev: %s", device.name)
+        log.info("Changing status for device: %s", device.name)
         try:
             tuya_device = self.create_tuya_device(device)
-            tuya_status = tuya_device.status()
-            if tuya_status.get('Error'):
-                log.info("Can't get tuya status: %s", tuya_status)
-                return False
-
-            switch_status = tuya_status['dps']['1']
+            switch_status = self.get_device_switch_status(tuya_device)
             if switch_status == status.value:
                 log.info("Device: %s is already %s (switch_status: %s). Pass.", device.name, status.name, switch_status)
                 return True
 
-            response = tuya_device.set_status(bool(status.value))
-            log.info("Device: %s status successfully changed. Response: %s", device.name, response)
+            if bool(status.value):
+                tuya_device.turn_on()
+            else:
+                tuya_device.turn_off()
+            log.info("Send signal to %s device: %s", "turn_on" if bool(status.value) else "turn_off", device.name)
             return True
         except Exception as e:
-            log.error("Problem with TuyaAPI with device %s", device.name, exc_info=e)
+            log.error("Problem with TuyaAPI device %s", device.name, exc_info=e)
             return False
 
     def get_current_power(self, device: Device) -> float:
